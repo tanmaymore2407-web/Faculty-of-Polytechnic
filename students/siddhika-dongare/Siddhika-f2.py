ab0ddef36deeba387ba8182470e5d98472b19df8
siddhikadongare98-code <siddhikadongare98@gmail.com>
2026-06-05 11:31:31 +0530

    Update Siddhika-f2.py

--- students/siddhika-dongare/Siddhika-f2.py
+++ students/siddhika-dongare/Siddhika-f2.py
@@ -1,1 +1,31 @@
+#problem1
+s = {'a', 'b', 'c', 'd'}
 
+s.update({'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+          'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+          'w', 'x', 'y', 'z'})
+
+print(s)
+#problem2
+s = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
+     'h', 'i', 'j', 'k', 'l', 'm', 'n',
+     'o', 'p', 'q', 'r', 's', 't', 'u',
+     'v', 'w', 'x', 'y', 'z'}
+
+c = 0
+
+for x in s:
+    if x == 'a':
+        c = c + 1
+    elif x == 'e':
+        c = c + 1
+    elif x == 'i':
+        c = c + 1
+    elif x == 'o':
+        c = c + 1
+    elif x == 'u':
+        c = c + 1
+    else:
+        continue
+
+print("Number of vowels =", c)
