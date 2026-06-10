b09e5c4ae6a6e8f66079e9945eccf98f173dfa98
gordesnehal01-lab <gordesnehal01@gmail.com>
2026-06-10 11:16:34 +0530

    Add rectangle area calculation in Solution5.py
    Added a new function to calculate the area of a rectangle.

--- students/gorde-snehal/Solution5.py
+++ students/gorde-snehal/Solution5.py
@@ -6,3 +6,15 @@ for i in range(N):
     students.append(name)
 
 print("Student List:", students)
+
+
+#Solution6.py
+def rectangle(l, b):
+    a = l * b
+    return a
+
+l = float(input("length: "))
+b = float(input("breadth: "))
+
+res = rectangle(l, b)
+print(res)
